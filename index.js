5cfbb04786962b1b200d773f03493d01cd8b5c27
CaioOta <caio.ryou@gmail.com>
2022-05-24 09:35:22 -0300

    feature: criando arquivo.js

--- index.js
+++ index.js
@@ -6,8 +6,5 @@ const port = 3000
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'views/index.html'))
 )
-app.get('/sobre', (req, res) =>
-  res.sendFile(path.join(__dirname, 'views/sobre.html'))
-)
 
 app.listen(port, () => console.info(`O servidor ta rodando na porta ${port}`))
